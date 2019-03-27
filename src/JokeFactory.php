44bf47635257074a0d045d8723844c1d83e2f833
Kofi Cypher <skcypher6@gmail.com>
2019-03-27 18:11:36 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,20 +2,20 @@
 
 namespace KofiCypher\ChuckNorrisJokes;
 
-class JokeFactory {
-  
+class JokeFactory
+{
     protected $jokes = [
         'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
         'Chuck Norris counted to infinity... Twice',
         'The First rule of Chuck Norris is: you do not talk about Chuck Norris',
-        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death'
+        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death',
     ];
 
     public function __construct(array $jokes = null)
     {
-      if($jokes){
-        $this->jokes = $jokes;
-      }
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
     }
 
     public function getRandomJoke()
