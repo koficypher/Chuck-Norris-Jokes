942ba7316463a172de2948f182d982a8ffdee0e3
Kofi Cypher <skcypher6@gmail.com>
2019-03-28 13:05:40 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -23,14 +23,14 @@ class JokeFactoryTest extends TestCase
     public function test_it_returns_a_random_joke()
     {
         $mock = new MockHandler([
-            new Response(200, [], '{ "type": "success", "value": { "id": 268, "joke": "Time waits for no man. Unless that man is John Doe." } }')  
+            new Response(200, [], '{ "type": "success", "value": { "id": 268, "joke": "Time waits for no man. Unless that man is John Doe." } }'),
         ]);
-        
+
         $handler = HandlerStack::create($mock);
         $client = new Client(['handler' => $handler]);
 
         $jokes = new JokeFactory($client);
-            
+
         $joke = $jokes->getRandomJoke();
 
         $this->assertSame('Time waits for no man. Unless that man is John Doe.', $joke);
